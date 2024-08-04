abd6d897a3efac120f438a91f219387a30f30198
abernier <antoine.bernier@gmail.com>
2024-08-04 14:43:08 +0200

    onClick client component

--- app/Foo.tsx
+++ app/Foo.tsx
@@ -1,10 +1,18 @@
+"use client";
+
+import { useState } from "react";
+
+const colors = ["red", "green", "blue"];
+
 export function Foo() {
+  const [index, setIndex] = useState(0);
+
   return (
-    <div>
+    <div onClick={() => setIndex((index) => (index + 1) % colors.length)}>
       <style>{`
         @scope {
           p {
-            color: red;
+            color: ${colors.at(index)};
           }
         }
       `}</style>
